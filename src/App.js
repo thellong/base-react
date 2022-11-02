9d042b0ee164549de5adffdc716146cd9f3b2858
thellong <firaxaxa@gmail.com>
2022-11-02 23:43:35 +0700

    state in react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
+import React from "react";
 
-function App() {
-  return (
-    <MyComponent></MyComponent>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "Long",
+  };
+
+  render() {
+    return <div>Hello {this.state.name}</div>;
+  }
 }
 
 export default App;
