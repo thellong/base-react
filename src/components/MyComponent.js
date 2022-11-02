29febf4bfcd7dd9413555ef2c312d6bebada45cc
thellong <firaxaxa@gmail.com>
2022-11-02 23:25:23 +0700

    understand component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+// Class component
+// function component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+  // JSX cho phep viet code js trong code html
+  render() {
+    return <p>My first component</p>;
+  }
+}
+
+export default MyComponent;
