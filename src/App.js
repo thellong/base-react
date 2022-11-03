6c551c1a940dd0b7ec58001525b765ef05d7c946
thellong <firaxaxa@gmail.com>
2022-11-03 09:51:09 +0700

    merge state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,32 @@ import React from "react";
 class App extends React.Component {
   state = {
     name: "Long",
+    age: 20,
   };
 
+  clickHandle(event) {
+    console.log("My name is: ", this.state.name, "and I'm ", this.state.age);
+
+    // merge state => react class
+    this.setState({
+      name: "Vippro",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
+  }
+
   render() {
-    return <div>Hello {this.state.name}</div>;
+    return (
+      <div>
+        Hello {this.state.name} I'm {this.state.age}
+        <button
+          onClick={(event) => {
+            this.clickHandle(event);
+          }}
+        >
+          Click me
+        </button>
+      </div>
+    );
   }
 }
 
