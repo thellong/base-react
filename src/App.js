3cf59c31431f269c0c6948b2ebd9084df14848b5
thellong <firaxaxa@gmail.com>
2022-11-04 21:48:25 +0700

    nested component

--- src/App.js
+++ src/App.js
@@ -2,49 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 import React from "react";
+import UserInfor from "./components/UserInfor";
 
 class App extends React.Component {
-  state = {
-    name: "Long",
-    age: 20,
-  };
-
-  clickHandle(event) {
-    console.log("My name is: ", this.state.name, "and I'm ", this.state.age);
-
-    // merge state => react class
-    this.setState({
-      name: "Vippro",
-      age: Math.floor(Math.random() * 100 + 1),
-    });
-  }
-
-  onChange = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-
   render() {
-    return (
-      <div>
-        Hello {this.state.name} I'm {this.state.age}
-        <form onSubmit={(event) => this.onSubmit(event)}>
-          <input
-            type="text"
-            onChange={(event) => {
-              this.onChange(event);
-            }}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    );
+    return <MyComponent />;
   }
 }
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,11 +2,16 @@
 // function component
 
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
   // JSX cho phep viet code js trong code html
   render() {
-    return <p>My first component</p>;
+    return (
+      <div>
+        <UserInfor />
+      </div>
+    );
   }
 }
 
