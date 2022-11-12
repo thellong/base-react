13e25829919c685fa8ae98058e71eb9a8bd637a3
thellong <firaxaxa@gmail.com>
2022-11-12 09:22:39 +0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import MyComponent from "./components/MyComponent";
-import React from "react";
-import AddUserInfor from "./components/AddUserInfor";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <MyComponent />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="app-container">Hello World!</div>;
+};
 
 export default App;
