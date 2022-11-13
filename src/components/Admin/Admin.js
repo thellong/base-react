ecde366d8117ae17033cf96fe31454cdf4f62737
thellong <firaxaxa@gmail.com>
2022-11-13 22:59:02 +0700

    react-pro-sidebar example

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,8 +1,12 @@
+import Sidebar from "./Sidebar";
+
 const Admin = () => {
   return (
-    <div>
-      Admin page
-      <div></div>
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <Sidebar />
+      </div>
+      <div className="admin-content"></div>
     </div>
   );
 };
