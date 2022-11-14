06c405936120be23681b2599a0e9ff047daf3cd5
thellong <therestofthepie@gmail.com>
2022-11-14 11:07:27 +0700

    pro sidebar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,18 @@
 import Sidebar from "./Sidebar";
+import { FaBars } from "react-icons/fa";
+import "./Admin.scss";
+import { useState } from "react";
 
 const Admin = () => {
+  const [isCollapsed, setCollapse] = useState(false);
   return (
     <div className="admin-container">
       <div className="admin-sidebar">
-        <Sidebar />
+        <Sidebar collapsed={isCollapsed} />
       </div>
-      <div className="admin-content"></div>
+      <div className="admin-content">
+        <FaBars onClick={() => setCollapse(!isCollapsed)} />
+      </div>
     </div>
   );
 };
--- src/components/Admin/Sidebar.js
+++ src/components/Admin/Sidebar.js
@@ -1,20 +1,102 @@
 import "react-pro-sidebar/dist/css/styles.css";
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { FaGem, FaHeart } from "react-icons/fa";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+import {
+  FaGem,
+  FaHeart,
+  FaTachometerAlt,
+  FaList,
+  FaGithub,
+  FaRegLaughWink,
+} from "react-icons/fa";
 
-const Sidebar = () => {
+import { AiOutlineSmile } from "react-icons/ai";
+
+import sidebarBg from "../../assets/sb.jpg";
+const Sidebar = (props) => {
+  const { image, collapsed, toggled, handleToggleSidebar } = props;
   return (
-    <div>
-      <ProSidebar>
-        <Menu iconShape="square">
-          <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-          <SubMenu title="Components" icon={<FaHeart />}>
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu>
+    <>
+      <ProSidebar
+        image={sidebarBg}
+        collapsed={collapsed}
+        toggled={toggled}
+        breakPoint="md"
+        onToggle={handleToggleSidebar}
+      >
+        <SidebarHeader>
+          <div
+            style={{
+              padding: "24px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              fontSize: 14,
+              letterSpacing: "1px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            <AiOutlineSmile size="3em" style={{ marginRight: "3px" }} />
+            Thellong
+          </div>
+        </SidebarHeader>
+        <SidebarContent>
+          <Menu iconShape="circle">
+            <MenuItem
+              icon={<FaTachometerAlt />}
+              suffix={<span className="badge red">New</span>}
+            >
+              Dashboard
+            </MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu icon={<FaRegLaughWink />} title="Features">
+              <MenuItem>Users manage</MenuItem>
+              <MenuItem>Quizes manage</MenuItem>
+              <MenuItem>Questions manage</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+        <SidebarFooter
+          style={{
+            textAlign: "center",
+          }}
+        >
+          <div
+            className="sidebar-btn-wrapper"
+            style={{
+              padding: "20px 24px",
+            }}
+          >
+            <a
+              href="https://github.com/thellong/base-react"
+              target="_blank"
+              className="sidebar-btn"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+              <span
+                style={{
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                View Source
+              </span>
+            </a>
+          </div>
+        </SidebarFooter>
       </ProSidebar>
-    </div>
+    </>
   );
 };
 
